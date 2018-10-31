fbe15ff04fa6cd855ecd5fda8b24313fffadc921
Edu Machado <eduardo30.machado@hotmail.com>
2018-10-30 23:21:40 -0300

    02:16:12 - Tutorial DDD - Eduardo Pires

---
+++ ProjetoModeloDDD.Application/AppServicebase.cs
@@ -0,0 +1,49 @@
+﻿using ProjetoModeloDDD.Application.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Application
+{
+    public class AppServicebase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IAppServiceBase<TEntity> _serviceBase;
+
+        public AppServicebase(IAppServiceBase<TEntity> serviceBase)
+        {
+            _serviceBase = serviceBase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
